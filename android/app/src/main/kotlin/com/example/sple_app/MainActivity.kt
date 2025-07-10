69e117659d5116ea2012f37d6bbecf9abc7723c1
Yousef4551 <examsple2@gmail.com>
2025-07-10 18:31:58 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/sple_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sple_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/sple/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sple.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
